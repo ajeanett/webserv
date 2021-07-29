436e6922ea92fca54728ffcd2fbe3b361a25a4d6
kfriese <kfriese@student.21-school.ru>
2021-07-29 14:36:27 +0300

    getResponder added

--- v4/HTTP/Responders/GetResponder.cpp
+++ v4/HTTP/Responders/GetResponder.cpp
@@ -24,84 +24,66 @@ GetResponder &GetResponder::operator=(GetResponder const &src)
 
 std::string GetResponder::respond(Request const &request, ParserConfig const &config, ServerData const &serverData) const
 {
+	std::cout << "request: " << request.getMethod() << " " << request.getLocation() << std::endl;
 	Response response;
 
-	LocationData const &locationData = serverData.getLocationData()[0];
+	LocationData const *currentLocation = nullptr;
 
-	std::string uri = locationData.getRoot() + request.getStartLine().find("location")->second;
-	while (!uri.empty() && uri[0] == ' ')
-		uri.erase(uri.begin());
+	const std::vector<LocationData> &locations = serverData.getLocationData();
+	for (std::vector<LocationData>::const_reverse_iterator it = locations.rbegin(); it != locations.rend(); ++it)
+	{
+		std::string const &location = it->getLocationPath();
+		if (location.compare(0, location.length(), request.getLocation(), 0, location.length()) == 0)
+		{
+			currentLocation = &(*it);
+			break;
+		}
+	}
+	if (currentLocation == nullptr)
+		return (response.error("404", "Not Found"));
+	std::string uri = currentLocation->getRoot() + request.getLocation();
 
-//	const std::vector<LocationData> &locationData = serverData.getLocationData();
-//	std::string requestLocation = request.getStartLine().find("location")->second;
-//	for (std::vector<LocationData>::const_reverse_iterator it = locationData.rbegin(); it != locationData.rend(); ++it)
+	if (uri.empty())
+		return (response.error("404", "Not Found"));
+
+//	if (request.getHeaders().find("Referer") != request.getHeaders().end())
 //	{
-//		std::string locationPath = it->getLocationPath();
-//		if (locationPath.compare(0, locationPath.length() - 1, requestLocation) == 0)
+//		std::string str = request.getHeaders().find("Referer")->second;
+//		if (str.length() != 0 && str[str.length() - 1] != '/')
 //		{
-//			uri = it->getRoot() + it->getFullPath();
-//			break;
+//			size_t n;
+//			n = str.find("//");
+//			if (n == std::string::npos)
+//				return (response.error("400", "Bad Request"));
+//			std::string::iterator it = str.begin();
+//			std::advance(it, n + 2);
+//			str.erase(str.begin(), it);
+//			n = str.find('/');
+//			if (n == std::string::npos)
+//				return (response.error("400", "Bad Request"));
+//			it = str.begin();
+//			std::advance(it, n);
+//			str.erase(str.begin(), it);
+//			uri += str;
 //		}
 //	}
 
-	if (uri.empty())
-	{
-		response.setStatus("404", "Not Found");
-		response.setBody("404, oops");
-		return (response.str());
-	}
-
-	if (request.getHeaders().find("Referer") != request.getHeaders().end())
-	{
-		std::string str = request.getHeaders().find("Referer")->second;
-		if (str.length() != 0 && str[str.length() - 1] != '/')
-		{
-			size_t n;
-			n = str.find("//");
-			if (n == std::string::npos)
-			{
-				// bad request
-			}
-			std::string::iterator it = str.begin();
-			std::advance(it, n + 2);
-			str.erase(str.begin(), it);
-			n = str.find('/');
-			if (n == std::string::npos)
-			{
-				// bad request
-			}
-			it = str.begin();
-			std::advance(it, n);
-			str.erase(str.begin(), it);
-			uri += str;
-		}
-	}
-
 	struct stat s;
 	if (stat(uri.c_str(), &s) < 0)
-	{
-		response.setStatus("404", "Not Found");
-		response.setBody("404, oops");
-		return (response.str());
-	}
+		return (response.error("404", "Not Found"));
 	else
 		response.setStatus("200", "OK");
 	if (s.st_mode & S_IFDIR)
 	{
 		if (uri.length() != 0 && uri[uri.length() - 1] != '/')
 			uri += '/';
-		std::string index = locationData.getIndex();
+		std::string index = currentLocation->getIndex();
 		while (!index.empty() && index[0] == ' ')
 			index.erase(index.begin());
 		uri += index;
 	}
 	if (stat(uri.c_str(), &s) < 0)
-	{
-		std::cout << "404" << std::endl;
-		response.setStatus("404", "Not Found");
-		response.setBody("404, oops");
-		return (response.str());
-	}
+		return (response.error("404", "Not Found"));
 
 	std::ifstream ifs(uri, std::ifstream::in);
 	std::stringstream html;
@@ -109,9 +91,17 @@ std::string GetResponder::respond(Request const &request, ParserConfig const &co
 	ifs.close();
 	std::string content = html.str();
 
-	response.getHeaders()["Content-Type"] = "text/html";
+	response.getHeaders()["Content-Type"] = "text/plain";
+	std::string const &requestLocation = request.getLocation();
+	if (requestLocation.find('.'))
+	{
+		size_t n = requestLocation.find_last_of('.');
+		std::string extension = requestLocation.substr(n + 1, requestLocation.length() - n);
+		response.getHeaders()["Content-Type"] = "text/" + extension;
+		std::cout << "result: " << response.getHeaders()["Content-Type"] << std::endl;
+	}
 	response.getHeaders()["Content-length"] = std::to_string(content.length());
-	response.getHeaders()["Connection"] = "close";
+	response.getHeaders()["Connection"] = "keep-alive";
 	response.setBody(content);
 
 	response.setStatus("200", "OK");
