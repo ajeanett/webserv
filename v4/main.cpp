8bda024b0a19c23e7c9d87c977d8e90278bd1c80
ajeanett <gvovus@mail.ru>
2021-07-30 20:00:30 +0300

    добавлена функция для отображения автоиндекса, пока прямо в мейн

--- v4/main.cpp
+++ v4/main.cpp
@@ -15,6 +15,71 @@
 #include "./server_engine/ServerEngine.hpp"
 #include "./server_engine/Date.hpp"
 
+#include <dirent.h>
+
+
+std::string get_dir_files(char* dir)
+{
+
+	std::string autoIndexBegin = "<!DOCTYPE html>\n"
+								 "<html>\n"
+								 "<head>\n"
+								 "<title>Index of /</title></head>\n"
+								 "<body bgcolor=\"white\">\n"
+								 "<h1>Index of /</h1>\n"
+								 "<hr>\n"
+								 "<pre>\n";
+
+	std::string autoIndexEnd = "</pre>\n"
+							   "<hr>\n"
+							   "</body>\n"
+							   "</html>\n";
+
+	struct dirent *dir_point;
+	DIR    *dir_descript;
+	std::string     str;
+	std::string     file_name;
+
+	dir_descript = opendir(dir);
+	if (dir_descript == NULL)
+		return ("ERROR");
+	str = "";
+	str+= autoIndexBegin;
+	while ((dir_point = readdir(dir_descript)))
+	{
+
+		if (dir_point->d_type != DT_DIR)
+		{
+			file_name = dir_point->d_name;
+		}
+		else
+		{
+			file_name = dir_point->d_name;
+			file_name += "/";
+		}
+		str+="     <a href=\"";
+		str+=file_name;
+		str+="\">";
+		str+=file_name;
+		str+="</a>";
+
+
+//        if (dir_point->d_type == DT_DIR)
+//        {
+//            printf("dir: %s",dir_point->d_name);
+//        }
+//        else if (dir_point->d_type)
+//        {
+//            printf(dir_point->d_name);
+//        }
+//        printf("\n");
+	}
+
+	str+= autoIndexEnd;
+	closedir(dir_descript);
+	return (str);
+}
+
 int main()
 {
 	ServerEngine serv;
---
+++ v4/python/test.php
@@ -0,0 +1,11 @@
+< ?php
+$str = <<<EOD
+Example of string
+Hello world!
+This is PHP!
+EOD;
+$name = 'Vova';
+echo <<<EOT
+My name is "$name".
+EOT;
+? >
