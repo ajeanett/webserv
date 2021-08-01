925f449dd8adddeced3bc6f0e7be579af1539f7a
ajeanett <gvovus@mail.ru>
2021-08-01 10:03:16 +0300

    Обновлен CGI. Добавление версии http и Content-Length в возвращаемом значении.

--- before_run.sh
+++ before_run.sh
@@ -4,5 +4,9 @@ echo "--- Copying 'html_pages', 'python', 'ex.conf' to cmake-build-debug/ ---"
 cp -r ./html_pages ./cmake-build-debug/
 cp -r ./cgi_scripts ./cmake-build-debug/
 cp -r ./ex.conf ./cmake-build-debug/
+cp -r ./testers ./cmake-build-debug/
+cp -r ./YoupiBanane ./cmake-build-debug/
+chmod 755 ./cmake-build-debug/cgi_scripts/*
+chmod 755 ./cmake-build-debug/testers/*
 echo "--- Done ---"
 echo
