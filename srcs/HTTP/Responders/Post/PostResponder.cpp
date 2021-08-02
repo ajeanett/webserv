47b44096694a9c4ea613424c2ecf28629f6a9905
kfriese <kfriese@student.21-school.ru>
2021-08-02 14:48:05 +0300

    trying youpi.bla

--- srcs/HTTP/Responders/Post/PostResponder.cpp
+++ srcs/HTTP/Responders/Post/PostResponder.cpp
@@ -38,7 +38,9 @@ std::string PostResponder::respond(const Request &request, const ParserConfig &c
 	const std::vector<LocationData> &locations = serverData.getLocationData();
 	for (std::vector<LocationData>::const_reverse_iterator it = locations.rbegin(); it != locations.rend(); ++it)
 	{
-		std::string const &location = it->getLocationPath();
+		std::string location = it->getLocationPath();
+		if (location.length() != 0 && location[0] != '/')
+			location.insert(0, "/");
 		if (location.compare(0, location.length(), request.getLocation(), 0, location.length()) == 0)
 		{
 			currentLocation = &(*it);
@@ -50,47 +52,25 @@ std::string PostResponder::respond(const Request &request, const ParserConfig &c
 	std::vector<std::string> const &locationMethods = currentLocation->getMethods();
 	if (!locationMethods.empty() && std::find(locationMethods.begin(), locationMethods.end(), request.getMethod()) == locationMethods.end())
 		return (response.error("405", "Method Not Allowed"));
-	std::string uri = currentLocation->getRoot() + request.getLocation();
-
-	if (uri.empty())
-		return (response.error("404", "Not Found"));
-
-//	if (request.getHeaders().find("Referer") != request.getHeaders().end())
-//	{
-//		std::string str = request.getHeaders().find("Referer")->second;
-//		if (str.length() != 0 && str[str.length() - 1] != '/')
-//		{
-//			size_t n;
-//			n = str.find("//");
-//			if (n == std::string::npos)
-//				return (response.error("400", "Bad Request"));
-//			std::string::iterator it = str.begin();
-//			std::advance(it, n + 2);
-//			str.erase(str.begin(), it);
-//			n = str.find('/');
-//			if (n == std::string::npos)
-//				return (response.error("400", "Bad Request"));
-//			it = str.begin();
-//			std::advance(it, n);
-//			str.erase(str.begin(), it);
-//			uri += str;
-//		}
-//	}
 
 	std::string content;
-	struct stat s;
-	if (stat(uri.c_str(), &s) < 0)
-		return (response.error("404", "Not Found"));
-	else
-		response.setStatus("200", "OK");
-	if (!currentLocation->getCgiExtension().empty())
+	if (!currentLocation->getCgiPath().empty())
 	{
+		std::cout << "request body: '" << request.getBody() << "'" << std::endl;
 		CGI cgi(request, serverData, currentLocation->getCgiPath(), currentLocation->getCgiExtension());
 		cgi.runCGI();
 		content = cgi.getBody();
 	}
 	else
 	{
+		std::string uri = currentLocation->getRoot() + request.getLocation();
+		if (uri.empty())
+			return (response.error("404", "Not Found"));
+		struct stat s;
+		if (stat(uri.c_str(), &s) < 0)
+			return (response.error("404", "Not Found"));
+		else
+			response.setStatus("200", "OK");
 		if (s.st_mode & S_IFDIR)
 		{
 			if (uri.length() != 0 && uri[uri.length() - 1] != '/')
@@ -121,6 +101,7 @@ std::string PostResponder::respond(const Request &request, const ParserConfig &c
 	response.getHeaders()["Content-length"] = std::to_string(content.length());
 	response.getHeaders()["Connection"] = "keep-alive";
 	response.setBody(content);
+	std::cout << "response body: '" << content << "'" << std::endl;
 
 	return (response.str());
 }
